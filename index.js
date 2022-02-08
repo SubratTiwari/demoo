d3b83126163c217620d4b9d95107a1e5f1678b0a
SubratTiwari <tsubrat301@gmail.com>
2022-02-09 01:08:00 +0530

    message

---
+++ index.js
@@ -0,0 +1,27 @@
+var a;
+a=10;
+console.log(a);
+
+a= "hello";
+console.log(a);
+console.log(typeof a);
+
+a= true;
+console.log(a);
+console.log(typeof a)
+
+a= null;
+console.log(a);
+console.log(typeof a);
+
+var str = "a";
+console.log(str);
+str= "how are you ?\n i am aeim";
+console.log(str);
+
+var b ='h! hope you guys are able grap the concepts';
+console.log(b);
+
+
+var num = 4500;
+console.log(`half of ${num} is ${num/2}`);
